761d6fb7be3a76f66ef958225fa679c7d4876ec3
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-06-12 09:03:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6281241056195','6281241056195','6281241056105']
+global.premium = ['6281241056195']
+global.packname = 'GilzTamvan'
+global.author = 'Malimg'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
